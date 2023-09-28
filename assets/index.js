454925eedcefc16485cd2363525c592594ade7b1
Kingwizard96 <calvarado1996@gmail.com>
2023-09-28 05:51:23 -0500

    api key is up and running

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,23 @@
 const API_KEY = `aLFWtvmRBhqufToLKCXJ2k0pLvSdsHhb1aroA73Y`
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
+
+function getWeather (){
+     fetch(API_URL)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("failed to retrieve data from NASA");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            console.log('Title:', data.title);
+            console.log('Explanation', data.explanation)
+            console.log('Image Url:', data.url);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+getWeather()        
